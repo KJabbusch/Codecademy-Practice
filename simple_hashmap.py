aa2d87c1c3eab64f14a8547da53629c1ad83d110
KJabbusch <kaj12@my.fsu.edu>
2021-05-03 16:04:01 -0700

    implementing a simple hash map in python

---
+++ simple_hashmap.py
@@ -0,0 +1,90 @@
+class HashMap:
+  def __init__(self, array_size):
+    self.array_size = array_size
+    self.array = [None for item in range(array_size)]
+
+  def hash(self, key, count_collisions=0):
+    key_bytes = key.encode()
+    hash_code = sum(key_bytes)
+    return hash_code + count_collisions
+
+  def compressor(self, hash_code):
+    return hash_code % self.array_size
+
+  def assign(self, key, value):
+    array_index = self.compressor(self.hash(key))
+    current_array_value = self.array[array_index]
+
+    if current_array_value is None:
+      self.array[array_index] = [key, value]
+      return
+
+    if current_array_value[0] == key:
+      self.array[array_index] = [key, value]
+      return
+
+    # Collision!
+
+    number_collisions = 1
+
+    while(current_array_value[0] != key):
+      new_hash_code = self.hash(key, number_collisions)
+      new_array_index = self.compressor(new_hash_code)
+      current_array_value = self.array[new_array_index]
+
+      if current_array_value is None:
+        self.array[new_array_index] = [key, value]
+        return
+
+      if current_array_value[0] == key:
+        self.array[new_array_index] = [key, value]
+        return
+
+      number_collisions += 1
+
+    return
+
+  def retrieve(self, key):
+    array_index = self.compressor(self.hash(key))
+    possible_return_value = self.array[array_index]
+
+    if possible_return_value is None:
+      return None
+
+    if possible_return_value[0] == key:
+      return possible_return_value[1]
+
+    retrieval_collisions = 1
+
+    while (possible_return_value[0] != key):
+      new_hash_code = self.hash(key, retrieval_collisions)
+      retrieving_array_index = self.compressor(new_hash_code)
+      possible_return_value = self.array[retrieving_array_index]
+
+      if possible_return_value is None:
+        return None
+
+      if possible_return_value[0] == key:
+        return possible_return_value[1]
+
+      retrieval_collisions += 1
+
+    return
+
+hash_map = HashMap(15)
+hash_map.assign('kim', 'seokjin')
+hash_map.assign('kim', 'namjoon')
+hash_map.assign('min', 'yoongi')
+hash_map.assign('kim', 'taehyung')
+hash_map.assign('jeon', 'jungkook')
+hash_map.assign('jung', 'hoseok')
+hash_map.assign('park', 'jimin')
+
+# We have implemented that only one value belongs to one key
+# taehyung overwrites the value for kim
+
+print(hash_map.retrieve('kim'))
+print(hash_map.retrieve('min'))
+print(hash_map.retrieve('jeon'))
+print(hash_map.retrieve('jung'))
+print(hash_map.retrieve('park'))
